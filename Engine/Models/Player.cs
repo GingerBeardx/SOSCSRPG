fdb78a3334b04bba02ac3df97dae0559475e8cc7
GingerBeardx <ecryptographer@gmail.com>
2020-10-05 06:30:48 -0600

    Bound the player info to the program window.

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Engine.Models
+﻿namespace Engine.Models
 {
     /// <summary>
     /// This class handles the player information.
@@ -12,7 +6,7 @@ namespace Engine.Models
     public class Player
     {
         public string Name { get; set; }
-        public string Class { get; set; }
+        public string CharacterClass { get; set; }
         public int HitPoints { get; set; }
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,13 +4,18 @@ namespace Engine.ViewModels
 {
     public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Eric";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
             CurrentPlayer.Gold = 1000000;
+
         }
     }
 }
