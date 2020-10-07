817731c37960ad46d3f43dfdd0d0f941acbcb80a
GingerBeardx <ecryptographer@gmail.com>
2020-10-06 19:54:27 -0600

    Created ability to move around the game world. Implemented buttons to make these movements and checks for available movement.

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -9,11 +9,11 @@ namespace Engine.Factories
             World newWorld = new World();
 
             newWorld.AddLocation(-2, -1, "Farmer's Field",
-                "There are rows of corn growing here,\nwith giant rats hiding between them.",
+                "There are rows of corn growing here, with giant rats hiding between them.",
                 "/Engine;component/Images/Locations/FarmFields.png");
 
             newWorld.AddLocation(-1, -1, "Farmer's House",
-                "This is the house of your neighbor,\nFarmer Ted.",
+                "This is the house of your neighbor, Farmer Ted.",
                 "/Engine;component/Images/Locations/Farmhouse.png");
 
             newWorld.AddLocation(0, -1, "Home",
@@ -29,19 +29,19 @@ namespace Engine.Factories
                 "/Engine;component/Images/Locations/TownSquare.png");
 
             newWorld.AddLocation(1, 0, "Town Gate",
-                "There is a gate here, protecting the town\nfrom giant spiders.",
+                "There is a gate here, protecting the town from giant spiders.",
                 "/Engine;component/Images/Locations/TownGate.png");
 
             newWorld.AddLocation(2, 0, "Spider Forest",
-                "The trees in this forest are covered with\nspider webs.",
+                "The trees in this forest are covered with spider webs.",
                 "/Engine;component/Images/Locations/SpiderForest.png");
 
             newWorld.AddLocation(0, 1, "Herbalist's hut",
-                "You see a small hut, with plants drying\nfrom the roof.",
+                "You see a small hut, with plants drying from the roof.",
                 "/Engine;component/Images/Locations/HerbalistsHut.png");
 
             newWorld.AddLocation(0, 2, "Herbalist's garden",
-                "There are many plants here, with snakes\nhiding behind them.",
+                "There are many plants here, with snakes hiding behind them.",
                 "/Engine;component/Images/Locations/HerbalistsGarden.png");
 
             return newWorld;
