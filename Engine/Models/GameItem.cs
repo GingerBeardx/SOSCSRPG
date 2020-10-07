22a5f843d6ef53a8fda8b79ec514349aa091128c
GingerBeardx <ecryptographer@gmail.com>
2020-10-07 06:25:36 -0600

    Worked on creating items in the game by creating game item class, a weapon subclass and the itemfactory class that handles the creation of items.

---
+++ Engine/Models/GameItem.cs
@@ -0,0 +1,21 @@
+﻿namespace Engine.Models
+{
+    public class GameItem
+    {
+        public int ItemTypeId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public GameItem(int itemTypeId, string name, int price)
+        {
+            ItemTypeId = itemTypeId;
+            Name = name;
+            Price = price;
+        }
+
+        public GameItem Clone()
+        {
+            return new GameItem(ItemTypeId, Name, Price);
+        }
+    }
+}
