ddedee2ec66845e3b0a2f073c291250cd7c57769
GingerBeardx <ecryptographer@gmail.com>
2020-10-07 18:39:05 -0600

    Created QuestFacotry, Item Quantity, and Quest classes in order to establish the creation of quests and items for rewards.

---
+++ Engine/Factories/QuestFactory.cs
@@ -0,0 +1,31 @@
+﻿using Engine.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Engine.Factories
+{
+    internal static class QuestFactory
+    {
+        private static readonly List<Quest> _quests = new List<Quest>();
+
+        static QuestFactory()
+        {
+            // Declare the items needed to complete the quest and its reward items
+            List<ItemQuantity> itemsToComplete = new List<ItemQuantity>();
+            List<ItemQuantity> rewardItems = new List<ItemQuantity>();
+
+            itemsToComplete.Add(new ItemQuantity(9001, 5));
+            rewardItems.Add(new ItemQuantity(1002, 1));
+
+            // Create the quest
+            _quests.Add(new Quest(1,
+                "Clear the herb garden",
+                "Defeat the snakes in the Herbalist's garden",
+                itemsToComplete,
+                25, 10, rewardItems));
+
+        }
+
+        internal static Quest GetQuestByID(int id) => _quests.FirstOrDefault(Quest => Quest.ID == id);
+    }
+}
