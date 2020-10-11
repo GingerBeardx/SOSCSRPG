329a5301929d6484082a7c1a86994bc06dd0f4cb
GingerBeardx <ecryptographer@gmail.com>
2020-10-10 19:33:17 -0600

    Added itemgrouping for inventories.

--- Engine/Models/GameItem.cs
+++ Engine/Models/GameItem.cs
@@ -5,17 +5,19 @@
         public int ItemTypeId { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
+        public bool IsUnique { get; set; }
 
-        public GameItem(int itemTypeId, string name, int price)
+        public GameItem(int itemTypeId, string name, int price, bool isUnique = false)
         {
             ItemTypeId = itemTypeId;
             Name = name;
             Price = price;
+            IsUnique = isUnique;
         }
 
         public GameItem Clone()
         {
-            return new GameItem(ItemTypeId, Name, Price);
+            return new GameItem(ItemTypeId, Name, Price, IsUnique);
         }
     }
 }
--- Engine/Models/Weapon.cs
+++ Engine/Models/Weapon.cs
@@ -5,7 +5,7 @@
         public int MinimumDamage { get; set; }
         public int MaximumDamage { get; set; }
         public Weapon(int itemTypeId, string name, int price, int minDamage, int maxDamage)
-            : base(itemTypeId, name, price) 
+            : base(itemTypeId, name, price, true) 
         {
             MinimumDamage = minDamage;
             MaximumDamage = maxDamage;
